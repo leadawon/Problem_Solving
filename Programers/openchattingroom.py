21da3a394d81594ef585fae3b7f23b4e4d52128d
leadawon <72075335+leadawon@users.noreply.github.com>
2022-12-15 10:53:23 +0900

    Update openchattingroom.py

--- Programers/openchattingroom.py
+++ Programers/openchattingroom.py
@@ -1,4 +1,5 @@
 '''
+<https://school.programmers.co.kr/learn/challenges?order=recent&page=1&partIds=12286>
 문제 설명
 오픈채팅방
 카카오톡 오픈채팅방에서는 친구가 아닌 사람들과 대화를 할 수 있는데, 본래 닉네임이 아닌 가상의 닉네임을 사용하여 채팅방에 들어갈 수 있다.
