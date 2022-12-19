7776b1aee8c9a8d570725c716d4fd52b17f4ee47
leadawon <72075335+leadawon@users.noreply.github.com>
2022-12-19 11:17:44 +0900

    Update week8.py

--- GOORM/week8.py
+++ GOORM/week8.py
@@ -13,4 +13,29 @@ for i in range(3,n+1):
 		dp[i]=dp[i-3]
 sys.stdout.write(f"{dp[n]}")
 
-#
+#9+3/4
+import sys
+from collections import defaultdict
+n,k = map(int,sys.stdin.readline().split())
+
+isin = defaultdict(int)
+timedic = defaultdict(int)
+
+for i in range(n):
+	time,name = sys.stdin.readline().split()
+	if isin[name] == 0:
+		h,m = map(int,time.split(":"))
+		isin[name] = 60*h+m
+	else:
+		h,m = map(int,time.split(":"))
+		if isin[name] <= 60*h+m:
+			timedic[name] += 60*h+m - isin[name]
+		else:
+			timedic[name] += 24*60 - isin[name] + 60*h+m
+		isin[name] = 0
+cnt = 0
+for key,val in timedic.items():
+
+	if val >= k*60:
+		cnt+=1
+sys.stdout.write(f"{cnt}")
