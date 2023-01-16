92dc012c6de6136e0f61bf549733441288e61896
Lee Dawon <dawon337@gmail.com>
2023-01-16 16:09:02 +0900

    Add CPP/BOJ/array

--- CPP/BOJ/array/2563.cpp
+++ CPP/BOJ/array/2563.cpp
@@ -6,16 +6,16 @@ int main(void){
     int colpap, row, col;
     cin >> colpap;
 
-    int arr[10][10] = {0};
+    int arr[100][100] = {0};
     for (int i=0; i<colpap; i++){
-        cin >> col >> row;
+        cin >> row >> col;
     
         for (int j=row ; j<row+10;j++){
-            if (j > 10){
+            if (j >= 100){
                 break;
             }
-            for (int k = 0 ; k < col + 10 ; k ++){
-                if (k > 10){
+            for (int k = col ; k < col + 10 ; k ++){
+                if (k >= 100){
                     break;
                 }
                 arr[j][k] = 1;
@@ -23,8 +23,8 @@ int main(void){
         }
     }
     colpap = 0;
-    for (int j = 0 ; j < 10 ; j ++){
-        for (int k = 0 ; k < 10 ; k ++){
+    for (int j = 0 ; j < 100; j ++){
+        for (int k = 0 ; k < 100 ; k ++){
             if (arr[j][k] == 1){
                 colpap++;
             }
