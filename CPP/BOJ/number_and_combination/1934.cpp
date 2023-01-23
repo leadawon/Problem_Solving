a31e0e4ff20bb1c6fd8e825f542db525355e467d
Lee Dawon <dawon337@gmail.com>
2023-01-23 18:09:51 +0900

    CPP/BOJ

---
+++ CPP/BOJ/number_and_combination/1934.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+	int c;
+	while (b != 0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int lcm(int a, int b)
+{
+    return a * b / gcd(a, b);
+}
+
+int main(void){
+	int t;
+    int a,b;
+
+	cin >> t;
+	for (int i=0;i<t;i++){
+        cin >> a >> b;
+        cout << lcm(a,b) << endl;
+    }
+
+
+
+    
+
+}
